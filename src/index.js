d16e2d4a5b957a63356e3f85137895c1d8c4d8b0
Dustin Hagemeier <dustin.hagemeier@web.de>
2017-05-29 01:11:31 +0200

    feat(lists): add ordered lists

--- src/index.js
+++ src/index.js
@@ -1,87 +1,82 @@
-var t = [];
-var j = 0;
-
-
-/**
- * outdent all rows by first as reference
- */
-const o = function (text) {
-  return text.replace(RegExp('^' + (text.match(/^\s+/) || '')[0], 'gm'), '');
-};
-
-/**
- * recursive list parser
- */
-const l = function (text) {
-  return text ?
-    `<ul>${o(text).replace(/[\+\-](.*)\n?((  .*\n?)*)/g, function (match, a, b) {return `<li>${a + l(b)}</li>`;})}</ul>`
-    : '';
-};
-
-const r = [
-
-  // BLOCK STUFF ===============================
-  // headlines
-  /^(#+) *(.*)$/gm,
-  function (match, h, text) {
-    return `<h${h.length}>${text}</h${h.length}>`
+var t = [],
+  j = 0,
+  i,
+  /**
+   * outdent all rows by first as reference
+   */
+  o = (text) => {
+    return text.replace(RegExp('^' + (text.match(/^\s+/) || '')[0], 'gm'), '');
   },
-
-  // unordered lists
-  /((^|\n)[\+\-](.*(\n  +.*)*))+/g,
-  l,
-
-  //pre format block
-  /(^|\n)```((.|\n)*)\n```/g,
-  "<pre>`$1`</pre>",
-
-  // extrude pre format inline
-  /`([^`]*)`/g,
-  function (match, text) {
-    t[++j] = text;
-    return '`' + j + '`';
+  /**
+   * recursive list parser
+   */
+  l = (text,temp) => {
+    temp = text.match(/\n?[+-]/m) ? 'ul' : 'ol';
+    return text ?
+      `<${temp}>${o(text).replace(/(?:[+-]|\d+\.)(.*)\n?((  .*\n?)*)/g, (match, a, b) => `<li>${a + l(b)}</li>`)}</${temp}>`
+      : '';
   },
+  // REPLACEMENT RULES
+  r = [
 
-  // INLINE STUFF ===============================
-  // image
-  /\!\[(.*)\]\(([^\s]*)( (.*))?\)/g,
-  '<img src="$2" alt="$1" title="$4"/>',
+    // BLOCK STUFF ===============================
 
-  // links
-  /\[(.*)\]\(([^\s]*)( (.*))?\)/g,
-  '<a href="$2" title="$4">$1</a>',
+    //pre format block
+    /(^|\n)```((.|\n)*)\n```/g,
+    "<pre>`$1`</pre>",
 
-  // bold
-  /(\*|_)\1([^\*|_]+)\1\1/g,
-  "<b>$2</b>",
+    // headlines
+    /^(#+) *(.*)$/gm,
+    (match, h, text) => `<h${h.length}>${text}</h${h.length}>`,
 
-  // italic
-  /(\*|_)([^\*|_]+)\1/g,
-  "<i>$2</i>",
+    // unordered lists
+    /(?:(^|\n)([+-]|\d+\.)(.*(\n  +.*)*))+/g,
+    l,
 
-  // replace remaining newlines with a <br>
-  /(  |\n)\n+/g,
-  "<br>",
+    // extrude pre format inline
+    /`([^`]*)`/g,
+    (match, text) => {
+      t[++j] = text;
+      return '`' + j + '`';
+    },
 
-  // inject classes
-  /(<[^>]+)>\."([^"]*)"/g,
-  '$1 class="$2">',
+    // INLINE STUFF ===============================
+    // image
+    /(\!)?\[(.*)\]\(([^\s]*)( (.*))?\)/g,
+    '<img src="$2" alt="$1" title="$4"/>',
 
-  // inject pre format inline texts
-  /`(\d+)`/g,
-  function (match, number) {
-    return t[number];
-  },
-];
+    // links
+    /\[(.*)\]\(([^\s]*)( (.*))?\)/g,
+    '<a href="$2" title="$4">$1</a>',
 
-const parse = function (text) {
-  var i = 0;
-  t = [];
-  j = 0;
-  while (i < r.length) {
-    text = text.replace(r[i++], r[i++]);
-  }
-  return text;
-};
+    // bold
+    /(\*|_)\1([^\*|_]+)\1\1/g,
+    "<b>$2</b>",
+
+    // italic
+    /(\*|_)([^\*|_]+)\1/g,
+    "<i>$2</i>",
+
+    // replace remaining newlines with a <br>
+    /(  |\n)\n+/g,
+    "<br>",
+
+    // inject classes
+    /(<[^>]+)>\."([^"]*)"/g,
+    '$1 class="$2">',
+
+    // inject pre format inline texts
+    /`(\d+)`/g,
+    (match, number) => t[number],
+  ],
+  parse = (text) => {
+    i = 0;
+    t = [];
+    j = 0;
+    while (i < r.length) {
+      text = text.replace(r[i++], r[i++]);
+    }
+    return text;
+  };
 
 module.exports = {rules: r, parse};
